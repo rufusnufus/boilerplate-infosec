2777a031eea7cea07223b80fdf2756f93546046f
rufusnufus <nufusrufus@gmail.com>
2021-01-06 15:32:18 +0300

    Hide Potentially Dangerous Information Using helmet.hidePoweredBy()

--- myApp.js
+++ myApp.js
@@ -50,6 +50,7 @@ var app = express();
 
 module.exports = app;
 var api = require('./server.js');
+app.use(helmet.hidePoweredBy())
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
