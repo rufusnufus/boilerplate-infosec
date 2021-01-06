b4335a2c7a3ea7861cc190f79e866dc616dd1bfc
rufusnufus <nufusrufus@gmail.com>
2021-01-06 15:54:09 +0300

    Disable Client-Side Caching

--- myApp.js
+++ myApp.js
@@ -13,6 +13,7 @@ app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({maxAge: 90*24*60*60, force: true}));
 app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
