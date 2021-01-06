e9a3cd9226085fbaefd35c9f5aab7d8ccee72b74
rufusnufus <nufusrufus@gmail.com>
2021-01-06 16:07:23 +0300

    Configure Helmet Using the 'parent'

--- myApp.js
+++ myApp.js
@@ -6,15 +6,22 @@ var app = express();
 
 module.exports = app;
 var api = require('./server.js');
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-app.use(helmet.hsts({maxAge: 90*24*60*60, force: true}));
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
-app.use(helmet.contentSecurityPolicy({directives: {defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"]}}));
+app.use(helmet({
+  frameguard: {
+    action: 'deny'
+  },
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "trusted-cdn.com"],
+    }
+  },
+  hsts: {
+    maxAge: 90*24*60*60, 
+    force: true
+  },
+  noCache: true,
+}));
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
