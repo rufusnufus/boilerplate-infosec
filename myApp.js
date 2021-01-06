9ff89a725e0f868d62d0ea54cf998e70a676bd39
rufusnufus <nufusrufus@gmail.com>
2021-01-06 15:37:53 +0300

    Mitigate the Risk of Cross Site Scripting

--- myApp.js
+++ myApp.js
@@ -52,6 +52,7 @@ module.exports = app;
 var api = require('./server.js');
 app.use(helmet.hidePoweredBy())
 app.use(helmet.frameguard({action: 'deny'}))
+app.use(helmet.xssFilter())
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
