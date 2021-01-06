ab7153ac5bf02ef9263065248a0167c127d380be
rufusnufus <nufusrufus@gmail.com>
2021-01-06 15:43:23 +0300

    Prevent IE from Opening Untrusted HTML

--- myApp.js
+++ myApp.js
@@ -10,6 +10,7 @@ app.use(helmet.hidePoweredBy())
 app.use(helmet.frameguard({action: 'deny'}))
 app.use(helmet.xssFilter())
 app.use(helmet.noSniff())
+app.use(helmet.ieNoOpen())
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
